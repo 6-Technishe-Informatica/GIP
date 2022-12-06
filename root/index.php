635e50f507e34bd5a4e6f559c540578e24b824fc
Manu-Devloo <95429732+Manu-Devloo@users.noreply.github.com>
2022-12-06 14:24:50 +0100

    nav & start main

--- root/index.php
+++ root/index.php
@@ -1,3 +1,26 @@
-<html>
-    testerggg
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style/index.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <?php include 'siteParts/nav.php'; ?>
+    </header>
+    <main>
+        <article>
+            <h2>Tech Point</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil accusantium corporis quidem, aut cupiditate voluptatum, adipisci ea consequuntur officiis temporibus eveniet perferendis laborum sunt fuga placeat sequi pariatur non impedit!</p>
+        </article>
+    </main>
+</body>
+
 </html>
---
+++ root/siteParts/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="index.php">Tech point</a>
+    <ul>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
