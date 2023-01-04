d42503c1bfc00de9fb3bc9514ea65ff5e642604c
Destroyerr1919 <89346457+Destroyerr1919@users.noreply.github.com>
2023-01-04 14:19:26 +0100

    blub

--- root/siteParts/nav.php
+++ root/siteParts/nav.php
@@ -1,5 +1,5 @@
 <nav>
-    <a href="index.php" id="logo">Tech point</a>
+    <a href="index.php" class="logo">Tech point</a>
     <ul>
         <li><a href="about.php" class="nav-item" >About</a></li>
         <li><a href="contact.php" class="nav-item ">Contact</a></li>
