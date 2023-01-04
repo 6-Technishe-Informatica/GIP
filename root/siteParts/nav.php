fa46ca554356bd66d5a05c814351726021709684
Manu-Devloo <95429732+Manu-Devloo@users.noreply.github.com>
2023-01-04 13:19:26 +0100

    nav // footer

--- root/siteParts/nav.php
+++ root/siteParts/nav.php
@@ -1,8 +1,8 @@
 <nav>
-    <a href="index.php">Tech point</a>
+    <a href="index.php" id="logo">Tech point</a>
     <ul>
-        <li><a href="about.php">About</a></li>
-        <li><a href="contact.php">Contact</a></li>
+        <li><a href="about.php" class="nav-item" >About</a></li>
+        <li><a href="contact.php" class="nav-item ">Contact</a></li>
     </ul>
 </nav>
 <hr>
