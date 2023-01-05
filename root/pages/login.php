591cb1dda3d0406bf61b4b41fbcf4dd630e872dd
Destroyerr1919 <89346457+Destroyerr1919@users.noreply.github.com>
2023-01-05 13:37:42 +0100

    login en register page

---
+++ root/pages/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../style/index.css">
+
+    <title>Tech point</title>
+</head>
+
+<body>
+    <header>
+        <?php include '../siteParts/nav.php'; ?>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <?php include '../siteParts/contact.php'; ?>
+    </footer>
+</body>
+
+</html>
--- root/siteParts/nav.php
+++ root/siteParts/nav.php
@@ -6,6 +6,8 @@
     <ul>
         <li class="nav-item" ><a href="about.php" class="underline-hover-effect">About</a></li>
         <li class="nav-item" ><a href="../pages/contact.php" class="underline-hover-effect">Contact</a></li>
+        <li class="nav-item" ><a href="../pages/signup.php" class="underline-hover-effect">Registreer</a></li>
+        <li class="nav-item" ><a href="../pages/login.php" class="underline-hover-effect">Log in</a></li>
         <li><a href="#"><img src="../images/winkelwagen.svg" alt="winkelwagen"></a></li>
     </ul>
 </nav>
