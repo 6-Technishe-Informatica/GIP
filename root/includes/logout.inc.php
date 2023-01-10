225582e34652768d0ce6992c3c1eff1fb333f615
Manu Devloo <devloo.prive@gmail.com>
2023-01-10 11:46:53 +0100

    del gip

--- root/includes/logout.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // logt de gebruiker uit
-    session_start(); // start de sessie
-    session_unset(); // verwijderd alle sessie variabelen
-    session_destroy(); // verwijderd de sessie
-
-    header("location: ../index.php"); // stuurt de gebruiker terug naar de index pagina
-    exit(); // zorgt ervoor dat de code stopt.
